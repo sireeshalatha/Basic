3f9908e6b56ca6c185e65754b15b3fdf2b9d15ca
sireeshalatha <sireeshalatha26@gmail.com>
2016-11-26 12:53:40 +0530

    commits

---
+++ src/main/java/org/company/cla/Program.java
@@ -0,0 +1,15 @@
+package org.company.cla;
+
+public class Program {
+	
+	public void m1(){
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+  public static void main(String[] args) {
+	Program p=new Program();
+	p.m1();
+}
+}
